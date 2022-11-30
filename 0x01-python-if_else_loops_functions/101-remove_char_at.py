5c43727185191be3a6e478399a8c401e34224354
lonebhen <ben.angmortey@gmail.com>
2022-11-30 13:47:14 +0000

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,13 +3,16 @@
 def remove_char_at(str, n):
     final = ''
 
-    for i in str:
-        if i == str[n]:
-            continue
-        else:
-            final += i
+    while n >= 0:
+        for i in str:
+            if i == str[n]:
+                continue
+            else:
+                final += i
 
-    return final
+        return final
+    return str
 
 
 # print(remove_char_at("Slim Shady", 4))
+# print(remove_char_at("Python", -2))
