fe24b1f0337a97d9f329abf3e3a3c60af1111e4e
lonebhen <ben.angmortey@gmail.com>
2022-12-02 13:21:43 +0000

    print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,5 @@ def print_list_integer(my_list=[]):
         print("{:d}".format(my_list[i]))
 
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+# my_list = [1, 2, 3, 4, 5]
+# print_list_integer(my_list)
