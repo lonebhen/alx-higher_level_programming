0a0b17604de4a2d5a686ad1348e75399a150227c
lonebhen <ben.angmortey@gmail.com>
2022-12-08 12:15:16 +0000

    task1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -9,7 +9,7 @@ def safe_print_integer(value):
     print()
 
 
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+# value = "School"
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
