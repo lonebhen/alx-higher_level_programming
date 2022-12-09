c30abc0421028beea39e4181f9fe8e7162fa6ed9
lonebhen <ben.angmortey@gmail.com>
2022-12-09 20:15:23 +0000

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,46 +3,21 @@
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a size of the square
-        __position (tuple): position of the square in 2D space
-    """
-
+    """ a square class"""
     def __init__(self, size=0, position=(0, 0)):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-            position (tuple): positoin of the square in 2D space
-        Returns:
-            None
-        """
+        """ Initialization"""
         self.size = size
         self.position = position
 
     def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
         return (self.__size) ** 2
 
     @property
     def size(self):
-        """getter of __size
-        Returns:
-            The size of the square
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter of __size
-        Args:
-            value (int): size of a side of the square
-        Returns:
-            None
-        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
@@ -52,7 +27,6 @@ class Square:
                 self.__size = value
 
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
@@ -65,20 +39,10 @@ class Square:
 
     @property
     def position(self):
-        """getter of __position
-        Returns:
-            The position of the square in 2D space
-        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """setter of __position
-        Args:
-            value (tuple): position of the square in 2D space
-        Returns:
-            None
-        """
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or value[0] < 0 or \
            type(value[1]) is not int or value[1] < 0:
