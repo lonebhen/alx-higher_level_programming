30e14490532806273dc749f965535d0aac8abfa2
lonebhen <ben.angmortey@gmail.com>
2022-12-09 19:56:23 +0000

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,13 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        for i in range(self.__size):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
