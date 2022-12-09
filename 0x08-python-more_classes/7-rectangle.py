b06f42a8dbc4110b63b2cb21a3ef597af0b88f6f
lonebhen <ben.angmortey@gmail.com>
2022-12-09 12:04:13 +0000

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """ Rectangle class"""
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = "C"
 
     def __init__(self, width=0, height=0):
         """Instantiation"""
