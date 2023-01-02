97c9cd83346d7e2f4c79f79e93a9978130667763
lonebhen <ben.angmortey@gmail.com>
2023-01-02 09:33:06 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,8 @@ class Base:
 
     def __init__(self, id=None):
         """Constructor"""
-        if id is not None:
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
             self.id = id
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
