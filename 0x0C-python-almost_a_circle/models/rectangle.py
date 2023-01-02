bb8c007de96f9e66c3b432a3b82e8e4736525acd
lonebhen <ben.angmortey@gmail.com>
2023-01-02 09:41:32 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from .base import Base
+
+"""A rectangle model class"""
+
+
+class Rectangle(Base):
+    """Rectangle Class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    # @property
+    # def width(self):
+    #     return self.width
+
+    # @width.setter
+    # def width(self, value):
