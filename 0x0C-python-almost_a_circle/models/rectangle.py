de0d34b838ea1f4c72e15310eec13921ab21f8cf
lonebhen <ben.angmortey@gmail.com>
2023-01-02 10:04:15 +0000

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -37,7 +37,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
